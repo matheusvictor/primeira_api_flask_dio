3cb8e48ab648a639d05bb0a6b100cb35177d9a74
Matheus Victor <matheusvictor.salles@gmail.com>
2022-07-12 09:56:14 -0300

    Otimiza endpoints da API
    Os métodos de atualização, exclusão e listagem de uma tarefa específica é feita através de uma única URI

--- main.py
+++ main.py
@@ -25,14 +25,28 @@ def index():
     return jsonify(lista_tarefas)
 
 
-@app.route('/tarefa/<int:id>', methods=['GET'])
-def listar_tarefa(id):
+@app.route('/tarefa/<int:id>', methods=['GET', 'DELETE', 'PUT'])
+def tarefa(id):
     try:
         response = lista_tarefas[id]
-        return jsonify(response)
+
+        if request.method == 'DELETE':
+            lista_tarefas.pop(id)
+            mensagem = f'Tarefa de ID {id} excluída com sucesso!'
+            response = {'status': f'{mensagem}'}
+        elif request.method == 'PUT':
+            status_atualizado = json.loads(request.data)
+            lista_tarefas[id]['status'] = status_atualizado['status']
+            mensagem = f'Status da tarefa de ID {id} atualizado com sucesso!'
+            response = {'status': f'{mensagem}'}
+
     except IndexError:
         mensagem = f'Não existe tarefa com ID {id} na lista de afazeres.'
         response = {'status': f'{mensagem}'}
+    except Exception:
+        mensagem = f'Erro desconhecido.'
+        response = {'status': f'{mensagem}'}
+
     return jsonify(response)
 
 
@@ -46,28 +60,6 @@ def cadastrar_tarefa():
         'status': payload['status']
     }
     return nova_tarefa
-
-
-@app.route('/atualizar_status/<int:id>', methods=['PUT'])
-def atualizar_status(id):
-    status_atualizado = json.loads(request.data)
-    lista_tarefas[id]['status'] = status_atualizado['status']
-    return jsonify(lista_tarefas[id])
-
-
-@app.route('/excluir/<int:id>', methods=['DELETE'])
-def excluir_tarefa(id):
-
-    try:
-        response = lista_tarefas[id]
-        mensagem = f'Tarefa de ID {id} excluída com sucesso.'
-        response = {'status': f'{mensagem}'}
-
-    except IndexError:
-        mensagem = f'Não existe tarefa com ID {id} na lista de afazeres.'
-        response = {'status': f'{mensagem}'}
-
-    return jsonify(response)
 
 
 if __name__ == '__main__':
